68a35417a1767032fb6444dbfd672f682a6da501
Tushar <tusharbhatt0135@gmail.com>
2025-11-07 16:45:30 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import DigitalClock from './code/digital-clock'
-import Dropdown from './code/dropdown.jsx'
 import "./App.css"
-import Cart from './code/cart.jsx'
-import Pagination from './code/pagination.jsx'
-import InfiniteScroll from './code/infinite-scroll.jsx'
-import Debouncethrottle from './code/debounce+throttle.jsx'
-import Check from './code/hoc.jsx'
-import InfiniteScroll2 from './code/infinite-scroll2.jsx'
 export default function App() {
   return (
-    <div className='space-y-12 p-2 flex flex-wrap gap-4'>
-      <DigitalClock/>
-      <Dropdown/>
-      <Cart/>
-      <Pagination/>
-      <InfiniteScroll/>
-      <Debouncethrottle/>
-      <Check/>
-      <InfiniteScroll2/>
+    <div>
+     Helllo world
     </div>
   )
 }
