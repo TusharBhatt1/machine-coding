54d197be4bfa5c69932b62d6403f7193094c03d3
Tushar <tusharbhatt0135@gmail.com>
2025-11-20 11:06:56 +0530

    groww.jsx

---
+++ src/code/output-based/groww.jsx
@@ -0,0 +1,55 @@
+// GROWW
+function example1() {
+    console.log("start");
+  
+    const promise1 = new Promise((resolve) => {
+      console.log(1);
+      resolve(2);
+      console.log(3);
+    });
+  
+    promise1.then((res) => {
+      console.log(res);
+    });
+  
+    console.log("end");
+  }
+  
+  function example2() {
+    const myObject = {
+      foo: "bar",
+  
+      func: function () {
+        const self = this;
+  
+        console.log("outer func: this.foo = " + this.foo); 
+        console.log("outer func: self.foo = " + self.foo);
+  
+        (function () {
+          console.log("inner func: this.foo = " + this.foo); 
+          console.log("inner func: self.foo = " + self.foo);
+        })();
+      },
+    };
+  
+    myObject.func();
+  }
+  
+  
+  function example3() {
+    function foo() {
+      let a = b = 0; 
+      a++;
+      return a;
+    }
+  
+    foo();
+  
+    console.log(typeof a); 
+    console.log(typeof b);
+  }
+
+  example1();
+  example2();
+  example3();
+  
