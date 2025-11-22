e55d0300382109422ee10502b8faa55dacecb844
Tushar <tusharbhatt0135@gmail.com>
2025-11-22 11:16:27 +0530

    useLocalStorage

---
+++ src/code/hooks/useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return {
+    value,
+    setValue,
+  };
+}
+
+//const {value,setValue}=useLocalStorage("test",123)
