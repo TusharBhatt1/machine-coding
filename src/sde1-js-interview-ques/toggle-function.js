561fb40585df2381610a85512f78bfc6cece3b89
Tushar <con-tushar.bhatt@pocketfm.com>
2026-01-22 21:05:59 +0530

    minor

--- src/sde1-js-interview-ques/toggle-function.js
+++ src/sde1-js-interview-ques/toggle-function.js
@@ -1,12 +1,13 @@
-function toggle(...args){
-    let index=0
-   return function(){
-        if(args.length===0) return 
-        if(index===args.length) index=0
+function toggle(...args) {
+    let index = 0
+    return function () {
+        if (args.length === 0) return
+        // if(index===args.length) index=0
+        index = (index + 1) % args.length
         return args[index++]
-   }
+    }
 }
-const onOff=toggle("on","off","on-off","off-on")
+const onOff = toggle("on", "off", "on-off", "off-on")
 
 console.log(onOff()) // on
 console.log(onOff()) // off
