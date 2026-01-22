1e00b0aba1d36d006fba22240721a6fbceec13a8
Tushar <con-tushar.bhatt@pocketfm.com>
2026-01-22 21:25:20 +0530

    minor

--- src/sde1-js-interview-ques/flatten-object.js
+++ src/sde1-js-interview-ques/flatten-object.js
@@ -12,3 +12,20 @@ function flatten(obj) {
 }
 
 console.log(flatten(obj))
+
+
+// const obj={a:"10",b:{x:"100",y:{val:200}}}
+// //ans => [1,2,3,7,10,11,100]
+// function flatten(obj,depth){
+//     let result={}
+//     if(depth===0) return obj
+//     for(let key in obj){
+//         if(typeof obj[key]==="object" ) {
+//             result={...result,...flatten(obj[key],depth-1)}
+//         }
+//         else result[key]=obj[key]
+//     }
+//     return result
+// }
+
+// console.log(flatten(obj,1))
